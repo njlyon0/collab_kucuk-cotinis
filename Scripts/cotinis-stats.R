0e1569842b853341e8307173788645138db64fef
Nick J Lyon <nicholasjlyon@gmail.com>
2021-09-08 15:50:44 -0500

    First pass statistics completed and working directory made universal

--- Scripts/cotinis-stats.R
+++ Scripts/cotinis-stats.R
@@ -10,7 +10,8 @@
 rm(list = ls())
 
 # Set working directory
-setwd("~/Documents/_Publications/2022_Kucuk_Cotinis/Kucuk-CotinisCollab")
+getwd() # should end in ".../Kucuk-CotinisCollab"
+myWD <- getwd()
 
 # Necessary libraries
 library(RRPP); library(tidyverse); library(vegan); library(ape)
@@ -18,45 +19,81 @@ library(RRPP); library(tidyverse); library(vegan); library(ape)
 ## ------------------------------------------- ##
       # Data Retrieval & Housekeeping ####
 ## ------------------------------------------- ##
+# Retrieve the relevant datasets
+alpha <- read.csv("./Data/alpha-diversity-data.csv")
+beta <- read.csv("./Data/beta-diversity-data.csv")
+wtd.frc.dist <- read.csv("./Data/wtd-unfrc-dist.csv")[-1]
+uwt.frc.dist <- read.csv("./Data/unwtd-unfrc-dist.csv")[-1]
 
+# Look at them to be sure nothing obvious is wrong
+str(alpha)
+str(beta)
+str(wtd.frc.dist)
+
+# Calculate the Bray Curtis and Jaccard distances
+bc.dist <- vegdist(beta[-c(1:5)], method = 'bray')
+jac.dist <- vegdist(beta[-c(1:5)], method = 'jaccard')
+
+# We also want to order the factor levels of what we're using as groups for PCoAs
+unique(beta$Stage.Gut)
+beta$Stage.Gut <- factor(beta$Stage.Gut, 
+                         levels = c("Larval paunch", "Larval ileum", "Larval midgut",
+                                    "Adult midgut", "Adult hindgut"))
+unique(beta$Stage.Gut)
+
+# Strip out the ID missing from the unifrac data
+frc.data <- beta %>%
+  filter(Sample.ID != "Amid48") %>%
+  as.data.frame()
+
+## ------------------------------------------- ##
+        # Alpha Diversity Tests ####
+## ------------------------------------------- ##
+# Incl. Chao, ACE, Simpson, and Shannon diversity
+str(alpha)
+
+# Chao1 Diversity
+  ## Fit the model
+chao <- lm.rrpp(Chao1 ~ Stage.Gut, data = alpha, iter = 9999)
+  ## Results?
+anova(chao)
+  ## If significant, run pairwise comparisons
+summary(pairwise(chao, groups = alpha$Stage.Gut))
+
+# Shannon Diversity
+shan <- lm.rrpp(Shannon ~ Stage.Gut, data = alpha, iter = 9999)
+anova(shan)
+summary(pairwise(shan, groups = alpha$Stage.Gut))
+
+# Simpson Diversity
+simp <- lm.rrpp(Simpson ~ Stage.Gut, data = alpha, iter = 9999)
+anova(simp)
+summary(pairwise(simp, groups = alpha$Stage.Gut))
+
+# Pielou's Evenness
+piel <- lm.rrpp(Pielous ~ Stage.Gut, data = alpha, iter = 9999)
+anova(piel)
+summary(pairwise(piel, groups = alpha$Stage.Gut))
+
+# ACE
+ace <- lm.rrpp(ACE ~ Stage.Gut, data = alpha, iter = 9999)
+anova(ace)
+summary(pairwise(ace, groups = alpha$Stage.Gut))
 
 ## ------------------------------------------- ##
           # Beta Diversity Tests ####
 ## ------------------------------------------- ##
-# Incl. Bray Curtis dissimilarity and Jaccard distance
+# Get a version of the beta diversity community that lacks all other values
+beta.resp <- as.matrix(select(beta, -Sample.ID:-Sex))
 
+# Now plug that whole dataframe in as the response variable
+beta.test <- lm.rrpp(beta.resp ~ Stage.Gut, data = beta)
 
-# Diversity ~ Life Stage + Gut Section, data = all
+# Look at the test
+anova(beta.test)
 
-# Initial perMANOVA
-anova(lm.rrpp(resp ~ factor, data = ref), effect.type = "F")
-## Interpretation: at least one group is significantly different from the others
-## Yes these groups don't mean anything, but it's still illustrative.
-
-# In reality would need to track down which groups are different from which (pairwise comparisons)
-## but we'll leave that alone here
-
-
-
-## ------------------------------------------- ##
-          # Alpha Diversity Tests ####
-## ------------------------------------------- ##
-# Incl. Chao, ACE, Simpson, and Shannon diversity
-
-
-# Diversity ~ Life Stage + Gut Section, data = all
-
-
-
-
-## ------------------------------------------- ##
-            # Larval Sex Tests ####
-## ------------------------------------------- ##
-# Same as above beta/alpha tests but subsetted for only larvae and incl. sex as explanatory variable
-
-
-# Diversity ~ Life Stage + Gut Section + Sex, data = larvae
-
+# Get pairwise results
+summary(pairwise(beta.test, groups = alpha$Stage.Gut))
 
 #END ####
 
