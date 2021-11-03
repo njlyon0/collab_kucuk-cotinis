26e1574a010c3e7c80c0bf90822a097a04a44606
Nick J Lyon <nicholasjlyon@gmail.com>
2021-11-03 13:38:19 -0500

    Created family- and phylum-level abundance bar plots

--- Scripts/cotinis-data-viz.R
+++ Scripts/cotinis-data-viz.R
@@ -20,14 +20,18 @@ library(tidyverse); library(vegan); library(ape); library(Rmisc)
       # Data Retrieval & Housekeeping ####
 ## -------------------------------------------- ##
 # Retrieve the relevant datasets
-alpha <- read.csv("./Data/alpha-diversity-data.csv")
-beta <- read.csv("./Data/beta-diversity-data.csv")
+alpha <- read.csv("./Data/Tidy Data/alpha-diversity-data.csv")
+beta <- read.csv("./Data/Tidy Data/beta-diversity-data.csv")
+fams <- read.csv("Data/Tidy Data/family-abun.csv")
+phlya <- read.csv("Data/Tidy Data/phylum-abun.csv")
 wtd.frc.dist <- read.csv("./Data/wtd-unfrc-dist.csv")[-1]
 uwt.frc.dist <- read.csv("./Data/unwtd-unfrc-dist.csv")[-1]
 
 # Look at them to be sure nothing obvious is wrong
 str(alpha)
 str(beta)
+str(fams)
+str(phyla)
 str(wtd.frc.dist)
 
 # Calculate the Bray Curtis and Jaccard distances
@@ -296,6 +300,178 @@ ggplot(chao, aes(y = Chao1, x = Stage.Gut, fill = Stage.Gut,
   pref_theme + theme(legend.position = 'none')
 ggsave(plot = last_plot(), "./Graphs/bar-chao.pdf",
        width = 5, height = 5, units = 'in')
+
+## -------------------------------------------- ##
+      # Family Abundance Bar Graphs ####
+## -------------------------------------------- ##
+# Summarize relevant subsets
+amid.fam <- fams %>%
+  filter(Stage.Gut == "Adult midgut") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Family")) %>%
+  as.data.frame()
+ahind.fam <- fams %>%
+  filter(Stage.Gut == "Adult hindgut") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Family")) %>%
+  as.data.frame()
+lmid.fam <- fams %>%
+  filter(Stage.Gut == "Larval midgut") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Family")) %>%
+  as.data.frame()
+paunch.fam <- fams %>%
+  filter(Stage.Gut == "Larval paunch") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Family")) %>%
+  as.data.frame()
+ileum.fam <- fams %>%
+  filter(Stage.Gut == "Larval ileum") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Family")) %>%
+  as.data.frame()
+
+# Make and save each graph
+  ## Adult midgut
+ggplot(amid.fam, aes(y = Abundance, x = reorder(Family, -Abundance),
+                     fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Family", y = "Adult Midgut Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/fam-abun-adult-midgut.pdf",
+       width = 6, height = 6, units = 'in')
+
+  ## Adult hindgut
+ggplot(ahind.fam, aes(y = Abundance, x = reorder(Family, -Abundance),
+                      fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Family", y = "Adult Hindgut Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/fam-abun-adult-hindgut.pdf",
+       width = 6, height = 6, units = 'in')
+
+  ## Larval midgut
+ggplot(lmid.fam, aes(y = Abundance, x = reorder(Family, -Abundance),
+                     fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Family", y = "Larval Midgut Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/fam-abun-larval-midgut.pdf",
+       width = 6, height = 6, units = 'in')
+
+  ## Larval paunch
+ggplot(paunch.fam, aes(y = Abundance, x = reorder(Family, -Abundance),
+                       fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Family", y = "Larval Paunch Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/fam-abun-larval-paunch.pdf",
+       width = 6, height = 6, units = 'in')
+
+  ## Larval ileum
+ggplot(ileum.fam, aes(y = Abundance, x = reorder(Family, -Abundance),
+                      fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Family", y = "Larval Ileum Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/fam-abun-larval-ileum.pdf",
+       width = 6, height = 6, units = 'in')
+
+## -------------------------------------------- ##
+        # Phylum Abundance Bar Graphs ####
+## -------------------------------------------- ##
+# Summarize relevant subsets
+amid.phyl <- fams %>%
+  filter(Stage.Gut == "Adult midgut") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Phylum")) %>%
+  as.data.frame()
+ahind.phyl <- fams %>%
+  filter(Stage.Gut == "Adult hindgut") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Phylum")) %>%
+  as.data.frame()
+lmid.phyl <- fams %>%
+  filter(Stage.Gut == "Larval midgut") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Phylum")) %>%
+  as.data.frame()
+paunch.phyl <- fams %>%
+  filter(Stage.Gut == "Larval paunch") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Phylum")) %>%
+  as.data.frame()
+ileum.phyl <- fams %>%
+  filter(Stage.Gut == "Larval ileum") %>%
+  summarySE(measurevar = "Abundance", groupvars = c("Stage.Gut", "Phylum")) %>%
+  as.data.frame()
+
+# Make and save each graph
+## Adult midgut
+ggplot(amid.phyl, aes(y = Abundance, x = reorder(Phylum, -Abundance),
+                      fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Phylum", y = "Adult Midgut Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/phyla-abun-adult-midgut.pdf",
+       width = 6, height = 6, units = 'in')
+
+## Adult hindgut
+ggplot(ahind.phyl, aes(y = Abundance, x = reorder(Phylum, -Abundance),
+                       fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Phylum", y = "Adult Hindgut Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/phyla-abun-adult-hindgut.pdf",
+       width = 6, height = 6, units = 'in')
+
+## Larval midgut
+ggplot(lmid.phyl, aes(y = Abundance, x = reorder(Phylum, -Abundance),
+                      fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Phylum", y = "Larval Midgut Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/phyla-abun-larval-midgut.pdf",
+       width = 6, height = 6, units = 'in')
+
+## Larval paunch
+ggplot(paunch.phyl, aes(y = Abundance, x = reorder(Phylum, -Abundance),
+                        fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Phylum", y = "Larval Paunch Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/phyla-abun-larval-paunch.pdf",
+       width = 6, height = 6, units = 'in')
+
+## Larval ileum
+ggplot(ileum.phyl, aes(y = Abundance, x = reorder(Phylum, -Abundance),
+                       fill = Stage.Gut, color = 'x')) +
+  geom_bar(stat = 'identity') +
+  geom_errorbar(aes(ymax = Abundance + se, ymin = Abundance - se), width = 0.2) +
+  scale_fill_manual(values = all.cols) +
+  scale_color_manual(values = 'black') +
+  labs(x = "Phylum", y = "Larval Ileum Abundance") +
+  pref_theme + theme(legend.position = 'none', axis.text.x = element_text(size = 8))
+ggsave(plot = last_plot(), "./Graphs/phyla-abun-larval-ileum.pdf",
+       width = 6, height = 6, units = 'in')
 
 # END ####
 
