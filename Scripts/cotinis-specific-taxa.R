94eb869a6db995d066d6f1a3ad255bcc2bd5c4a0
Nick J Lyon <nicholasjlyon@gmail.com>
2022-06-10 10:32:59 -0500

    Pre-pivot away from inclusion of 'other' bars

--- Scripts/cotinis-specific-taxa.R
+++ Scripts/cotinis-specific-taxa.R
@@ -27,19 +27,21 @@ beta <- read.csv(file.path("Data", "Tidy Data", "beta-diversity-data.csv"))
 # Create directory to save these to
 dir.create("Special Taxa Graphs", showWarnings = F)
 
-# We also want to customize some plotting aesthetics for our ggplot plots that we can do here
-all.cols <- c("Larval paunch" = "#a50026", "Larval ileum" = "#f46d43",
-              "Larval midgut" = "#fee090",
-              "Adult midgut" = "#abd9e9", "Adult hindgut" = "#4575b4")
-sex.shps <- c("male" = 24, "female" = 25, "larva"= 21)
-dodge <- position_dodge(width = 0.5)
+# We also want to make a standard ggplot2 theme
 pref_theme <- theme_classic() +
   theme(legend.title = element_blank(),
-        legend.position = 'none',
         axis.title = element_text(size = 15),
         axis.text.y = element_text(size = 11),
         axis.text.x = element_text(size = 13, angle = 35, hjust = 1),
         legend.background = element_rect(fill = alpha('black', 0)))
+
+# Identify the desired order of gut regions for the factor levels
+gut_levels <- c("Larval paunch", "Larval ileum", "Larval midgut",
+                "Adult midgut", "Adult hindgut")
+
+# Create vector for coloring as well
+## Order is one white then order of 'gut_levels' object above
+color_vec <- c("#a50026", "#f46d43", "#fee090", "#abd9e9", "#4575b4")
 
 ## -------------------------------------------------------------- ##
                   # Specific Taxa Graphing ####
@@ -55,7 +57,8 @@ desired_taxa <- c("Gilliamella", "Bacillus", "Turicibacter", "Geobacter",
                   "Desulfovibrionaceae", "Lachnospiraceae")
 
 # Loop through that vector to summarize, plot, and export for each
-for(k in 1:length(desired_taxa)){
+# for(k in 1:length(desired_taxa)){
+for(k in 1){
   
   # Identify taxon
   special_taxon <- desired_taxa[k]
@@ -63,37 +66,56 @@ for(k in 1:length(desired_taxa)){
   # Summarize the dataframe as needed
   sub_df <- beta %>%
     # Pivot to long format
-    pivot_longer(-Sample.ID:-Sex, names_to = 'Taxon', values_to = 'Abundance') %>%
+    tidyr::pivot_longer(-Sample.ID:-Sex, names_to = 'Taxon',
+                        values_to = 'Abundance') %>%
+    # Coerce all IDs other than the desired one into 'other'
+    dplyr::mutate(Taxon_Simp = dplyr::case_when(
+      stringr::str_detect(string = Taxon, pattern = special_taxon) ~ special_taxon,
+      TRUE ~ "Other") ) %>%
     # Keep only the genus of interest
-    filter(str_detect(string = Taxon, pattern = special_taxon)) %>%
+    # filter() %>%
     # Sum across samples within Stage.Gut
-    group_by(Stage.Gut) %>%
+    group_by(Stage.Gut, Taxon_Simp) %>%
     dplyr::summarise(Abundance = sum(Abundance, na.rm = T)) %>%
     # Re-level the Stage.Gut column
-    dplyr::mutate(Stage.Gut = factor(Stage.Gut,
-                                     levels = c("Larval paunch", "Larval ileum", "Larval midgut",
-                                                "Adult midgut", "Adult hindgut"))) %>%
+    dplyr::mutate(Stage.Gut = factor(Stage.Gut, levels = gut_levels)) %>%
+    # Calculate relative abundance
+    dplyr::group_by(Stage.Gut) %>%
+    dplyr::mutate(
+      total = sum(Abundance, na.rm = T),
+      percAbun = (Abundance / total) * 100,
+      # Create column to fill by
+      Gut_Taxon_raw = ifelse(Taxon_Simp == "Other",
+                             yes = "Other",
+                             no = paste(Stage.Gut, Taxon_Simp, sep = "_")),
+      Gut_Taxon = factor(Gut_Taxon_raw,
+                         levels = c("Other",
+                                    paste0(gut_levels, "_", special_taxon)))
+      ) %>%
     # Return dataframe
     as.data.frame()
   
-  # Assemble y-axis label
+ # Assemble y-axis label
   if(str_detect(string = special_taxon, pattern = "aceae") == FALSE){
-    y_axis_label <- paste(special_taxon, "spp. Abundance") }
+    y_axis_label <- paste(special_taxon, "spp. Relative Abundance (%)") }
   if(str_detect(string = special_taxon, pattern = "aceae") == TRUE){
-    y_axis_label <- paste(special_taxon, "Abundance") }
-  
-  # Plot that data
-  sub_plot <- ggplot(sub_df, aes(y = Abundance, x = Stage.Gut, fill = Stage.Gut, color = 'x')) +
+    y_axis_label <- paste(special_taxon, "Relative Abundance (%)") }
+
+  # Plot that subset of the data
+  sub_plot <- ggplot(sub_df, aes(y = percAbun, x = Stage.Gut,
+                                 fill = Gut_Taxon, color = 'x')) +
     geom_bar(stat = 'identity') +
-    scale_fill_manual(values = all.cols) +
+    scale_fill_manual(values = color_vec,
+                      labels = c("Other Taxa", special_taxon)) +
     scale_color_manual(values = 'black') +
     labs(x = "Life Stage & Gut Region", y = y_axis_label) +
-    pref_theme
+    pref_theme +
+    guides(color = 'none')
   
   # Save that plot
   ggplot2::ggsave(file.path("Special Taxa Graphs",
                             paste0(special_taxon, "-Figure.tiff")),
-                  width = 4, height = 4, plot = sub_plot)
+                  width = 4, height = 5, plot = sub_plot)
   
   # Alert the user which graph was produced
   message(special_taxon, " graphic produced. ", length(desired_taxa) - k, " remaining." )
